9b4beb176d4d9855a0c4fee4731823b678647b21
Nik McPhail <nikolasstevenmcphail@gmail.com>
2025-06-03 17:22:14 -0400

    Initial dotfiles.

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set fish_greeting
+
+zoxide init fish | source
+source (/usr/bin/starship init fish --print-full-init | psub)
